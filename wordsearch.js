e75701c1696d2f6f5f5d54694b9ffbb82b390d87
Jason Long <jason.long77@hotmail.com>
2020-09-23 21:23:34 +0000

    Finished wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
 const wordSearch = (letters, word) => { 
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    
+    for (let line of horizontalJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
     }
+    for (let line of verticalJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
-module.exports = wordSearch
+const transpose = function(letters) {
+    let result = [];
+    for (let array of letters[0]) {
+      result.push([]);
+    }
+    for (let i = 0; i < letters.length; i++) {
+      for (let j = 0; j < letters[i].length; j++) {
+        result[j].push(letters[i][j]);
+      }
+    
+    }
+    console.log("Transpose result: ", result);
+    return result;
+};
+
+
+module.exports = wordSearch;
