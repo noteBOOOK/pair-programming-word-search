a36c044508d10d6ce55c5df8845bce0e6165153b
Jason Long <jason.long77@hotmail.com>
2020-09-23 21:26:17 +0000

    Added failed vertical test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -52,4 +52,20 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'P'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'E'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'W'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'P'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'E'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'R'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'PEPPER')
+
+    assert.isFalse(result);
+  });
 });
