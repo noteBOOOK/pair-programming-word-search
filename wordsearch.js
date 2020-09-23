9a248ae6292049e2fb85000523d26b9c6f571b20
Jason Long <jason.long77@hotmail.com>
2020-09-23 21:42:45 +0000

    Cleaned up the code with lint

--- wordsearch.js
+++ wordsearch.js
@@ -1,33 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const verticalJoin = transpose(letters).map(ls => ls.join(''));
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    
-    for (let line of horizontalJoin) {
-        if (line.includes(word)) {
-            return true;
-        }
-    }
-    for (let line of verticalJoin) {
-        if (line.includes(word)) {
-            return true;
-        }
-    }
-    return false;
-}
-
 const transpose = function(letters) {
-    let result = [];
-    for (let array of letters[0]) {
-      result.push([]);
+  let result = [];
+  for (const line of letters[0]) {
+    result.push([]);
+  }
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      result[j].push(letters[i][j]);
     }
-    for (let i = 0; i < letters.length; i++) {
-      for (let j = 0; j < letters[i].length; j++) {
-        result[j].push(letters[i][j]);
-      }
-    
-    }
-    return result;
+  
+  }
+  return result;
 };
 
+const wordSearch = (letters, word) => {
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  for (const line of horizontalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+  for (const line of verticalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = wordSearch;
